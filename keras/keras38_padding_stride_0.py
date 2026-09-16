1c2d955a01c59e8d351cae0d64eed33c70c88ba2
Joarthvr <ioi456859@gmail.com>
2026-09-16 17:25:11 +0900

    study: CNN stride & padding

---
+++ keras/keras38_padding_stride_0.py
@@ -0,0 +1,24 @@
+import np as np
+from tensorflow.keras.layers import Conv2D, Input
+from tensorflow.keras.models import Sequential
+
+#! 2. 모델구성
+model = Sequential()
+model.add(Input((10, 10, 1)))
+model.add(
+    Conv2D(
+        10,
+        (2, 2),  # (9,9,10)
+        strides=1,  # 기본값
+        padding='valid',  # 기본값
+    )
+)
+model.add(
+    Conv2D(
+        filters=9,
+        kernel_size=(3, 3),  # (7,7,9)
+        strides=1,
+        padding='valid',
+    )
+)
+model.summary()
